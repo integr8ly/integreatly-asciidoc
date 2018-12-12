c14c276f84b65cda3323e83e2e13d19d5e3763fa
Peter Braun <pbraun@redhat.com>
2018-12-12 10:54:43 +0100

    verify directories and json schema

--- bin/iad.js
+++ bin/iad.js
@@ -3,6 +3,11 @@
 const program = require('commander');
 const { readFile } = require('fs');
 const { check } = require('../dist/bundle.node');
+const { AdocCollector, verifyDirectoryStructure } = require('./fileUtils');
+const queue = require('async.queue');
+var Ajv = require('ajv');
+var ajv = new Ajv();
+var validateJson = ajv.compile(require('./resources/schema'));
 
 const BANNER = 'Integreatly Asciidoc';
 
@@ -21,6 +26,12 @@ function readFromFile(path) {
   });
 }
 
+/**
+ * Read from stdin. Allows the cli to be used with cat:
+ * cat file.adoc | ./iad
+ * @param stream stdin
+ * @returns {Promise<any>}
+ */
 function readFromStream(stream) {
   return new Promise((resolve, reject) => {
     const data = [];
@@ -36,6 +47,63 @@ function readFromStream(stream) {
   });
 }
 
+/**
+ * Processes a directory and performs the following checks
+ * 1) subdirs for walkthroughs present
+ * 2) walkthrough.adoc present in every subdir
+ * 3) walkthrough.json present in every subdir
+ * 4) all adoc files valid
+ * 5) all json files valid
+ * @param dir The directory where
+ * @param context
+ */
+function processDirectory(dir, context) {
+  const collector = new AdocCollector(dir);
+
+  // Process all adoc files sequentially
+  const adocQueue = queue((doc, cb) => {
+    readFromFile(doc).then(raw => {
+      console.log(`processing ${doc}`);
+      const success = check(raw, context);
+      if (!success) {
+        process.exit(1);
+      }
+      return cb();
+    }).catch(handleError);
+  });
+
+  // Process all json files sequentially
+  const jsonQueue = queue((doc, cb) => {
+    console.log(`processing ${doc}`);
+    let json;
+    try {
+      json = require(doc);
+    } catch (_) {
+      console.error(`ERROR Invalid or empty JSON file`);
+      process.exit(1);
+    }
+
+    const success = validateJson(json);
+    if (!success) {
+      console.error('ERROR');
+      console.error(validateJson.errors);
+      process.exit(1);
+    }
+    return cb();
+  });
+
+  collector.on('adoc', file => {
+    adocQueue.push(file)
+  });
+
+  collector.on('json', file => {
+    jsonQueue.push(file);
+  });
+
+  // Collect all json and adoc files
+  collector.run();
+}
+
 function wrapWithContext(context) {
   return function (raw) {
     const success = check(raw, context);
@@ -45,17 +113,27 @@ function wrapWithContext(context) {
   }
 }
 
+function handleError(err) {
+  console.error(err);
+  process.exit(1);
+}
+
 function main () {
   program
     .version(getVersion())
     .option('-f --file <file>', 'Input file')
+    .option('-d --directory <directory>', 'Walkthroughs path')
     .option('-W --warnings <level>', 'Treat warnings as errors when set to `error`')
     .parse(process.argv);
 
-  if (program.file) {
-    readFromFile(program.file).then(wrapWithContext(program)).catch(console.error);
+  if (program.directory) {
+    verifyDirectoryStructure(program.directory)
+      .then(dir => processDirectory(dir, program))
+      .catch(handleError);
+  } else if (program.file) {
+    readFromFile(program.file).then(wrapWithContext(program)).catch(handleError);
   } else {
-    readFromStream(process.stdin).then(wrapWithContext(program)).catch(console.error);
+    readFromStream(process.stdin).then(wrapWithContext(program)).catch(handleError);
   }
 }
 
